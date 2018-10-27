e84e1e07e17ef8e185070a433c55327a2bd33d95
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2018-10-27 18:01:31 +0530

    Update src/binarySearchTree/ConstructBSTFromPreOrder.java

--- src/binarySearchTree/ConstructBSTFromPreOrder.java
+++ src/binarySearchTree/ConstructBSTFromPreOrder.java
@@ -1,5 +1,7 @@
 package binarySearchTree;
 
+import java.util.Stack;
+
 public class ConstructBSTFromPreOrder {
 	
 	MyBSTNode root;
@@ -12,16 +14,46 @@ public class ConstructBSTFromPreOrder {
 		ConstructBSTFromPreOrder obj = new ConstructBSTFromPreOrder();
 //		int[] arr = {10, 5, 1, 7, 40, 50};
 		int[] arr = {10, 3, 1, 9, 18, 15, 13, 17, 20, 19, 27};
-		long startTime = System.nanoTime();
 		obj.doOperation(arr);
-		long endTime = System.nanoTime();
-		obj.printInOrder();
-		System.out.println("Total time taken : "+(endTime - startTime));
 	}
 	
 	private void doOperation(int[] arr) {
+		long startTime = System.nanoTime();
 		root = null;
 		root = constructBstByPreOrder(arr, 0, arr.length-1);
+		long endTime = System.nanoTime();
+		printInOrder();
+		System.out.println("Total time taken for O(n^2) : "+ (endTime - startTime));
+		
+		System.out.println("------------");
+		
+		long startTimeByStack = System.nanoTime();
+		root = null;
+		root = constructBstByPreOrderByStack(arr, arr.length);
+		long endTimeByStack = System.nanoTime();
+		printInOrder();
+		System.out.println("Total time taken by Stack : "+ (endTimeByStack - startTimeByStack));
+	}
+
+	private MyBSTNode constructBstByPreOrderByStack(int[] arr, int size) {
+		Stack<MyBSTNode> stackObj = new Stack<MyBSTNode>();
+		MyBSTNode root = new MyBSTNode(arr[0]);
+		stackObj.push(root);
+		for (int i = 1; i < size; i++) {
+			MyBSTNode tempNode = null;
+			while (!stackObj.isEmpty() && arr[i] > stackObj.peek().data) {
+				tempNode = stackObj.pop();
+			}
+			if (tempNode != null) {
+				tempNode.right = new MyBSTNode(arr[i]);
+				stackObj.push(tempNode.right);
+			} else {
+				tempNode = stackObj.peek();
+				tempNode.left = new MyBSTNode(arr[i]);
+				stackObj.push(tempNode.left);
+			}
+		}
+		return root;
 	}
 
 	private MyBSTNode constructBstByPreOrder(int[] arr, int start, int end) {
@@ -31,10 +63,8 @@ public class ConstructBSTFromPreOrder {
 		MyBSTNode node = new MyBSTNode(arr[start]);
 		int nextGreaterIndex = getNextGreaterElement(arr, start, end);
 		//create left & right of node by recursion
-		if (start+1 < nextGreaterIndex) {
-			node.left = constructBstByPreOrder(arr, start+1, nextGreaterIndex-1);
-		}
-		if (nextGreaterIndex >= start+1) {
+		node.left = constructBstByPreOrder(arr, start+1, (/*nextGreaterIndex > start+1*/ nextGreaterIndex != -1) ? nextGreaterIndex-1 : end);
+		if (nextGreaterIndex > start) {
 			node.right = constructBstByPreOrder(arr, nextGreaterIndex, end);
 		}
 		return node;
