6f1aeab8054fa238b0f8ac42fe1758ef880baa34
Manohar51 <manohar.glimpse@gmail.com>
2018-10-29 17:57:40 +0530

    Update src/binarySearchTree/MyAVLTree.java

--- src/binarySearchTree/MyAVLTree.java
+++ src/binarySearchTree/MyAVLTree.java
@@ -29,4 +29,22 @@ public class MyAVLTree {
 		return node != null ? node.height : -1;
 	}
 	
+	private AVLNode leftRotation(AVLNode k2) {
+		AVLNode k1 = k2.right;
+		k2.right = k1.left;
+		k1.left = k2;
+		k2.height = Math.max(k2.left.height, k2.right.height) + 1;
+		k1.height = Math.max(k2.height, k1.right.height) + 1;
+		return k1;
+	}
+	
+	private AVLNode rightRotation(AVLNode k2) {
+		AVLNode k1 = k2.left;
+		k2.left = k1.right;
+		k1.right = k2;
+		k2.height = Math.max(k2.left.height, k2.right.height) + 1;
+		k1.height = Math.max(k1.left.height, k2.height) + 1;
+		return k1;
+	}
+	
 }
