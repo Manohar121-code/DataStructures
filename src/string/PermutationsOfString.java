90c1415f04e490e2e8040267d89a937ed40d5e82
Manohar51 <manohar.glimpse@gmail.com>
2018-11-02 17:03:40 +0530

    String Permutations

--- src/string/PermutationsOfString.java
+++ src/string/PermutationsOfString.java
@@ -3,13 +3,23 @@ package string;
 public class PermutationsOfString {
 	public static void main(String[] args) {
 		PermutationsOfString obj = new PermutationsOfString();
-		String s = "ABC";
+		String s = "ABCD";
 		long startTime = System.nanoTime();
-		obj.doOperation(s);
-		System.out.println("\nTime taken " + (System.nanoTime() - startTime));
+		obj.doOperation("", s);
+		long endTime = System.nanoTime();
+		System.out.println("Time taken " + (endTime - startTime));
 	}
 
-	private void doOperation(String s) {
+	private void doOperation(String subStr, String s) {
+		if (s.length() == 0) {
+			System.out.println(subStr);
+		}
+		for (int i = 0; i < s.length(); i++) {
+			doOperation(subStr + s.charAt(i), s.substring(0, i)+s.substring(i+1, s.length()));
+		}
+	}
+	
+	/*private void doOperation(String s) {
 		String t = "";
 		if (s.length() == 1) {
 //			System.out.print(String.valueOf(s.charAt(0)));
@@ -19,5 +29,5 @@ public class PermutationsOfString {
 			doOperation(s.substring(0, i)+s.substring(i+1, s.length()));
 			System.out.print(" ");
 		}
-	}
+	}*/
 }
