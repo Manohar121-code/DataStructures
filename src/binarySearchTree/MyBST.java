ad284eb08282c8a08481dab931b7b732855e30b9
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2018-11-03 11:52:03 +0530

    Delete() Optimized way in mybst.java

--- src/binarySearchTree/MyBST.java
+++ src/binarySearchTree/MyBST.java
@@ -1,8 +1,10 @@
 package binarySearchTree;
 
+import java.util.Scanner;
+
 public class MyBST {
 	MyBSTNode root;
-	
+
 	public void insert(int data) {
 		if (root == null) {
 			root = new MyBSTNode(data);
@@ -10,11 +12,12 @@ public class MyBST {
 		}
 		insert(root, data);
 	}
+
 	public MyBSTNode insert(MyBSTNode node, int data) {
 		if (node == null) {
 			return node = new MyBSTNode(data);
 		}
-		
+
 		if (data < node.data) {
 			node.left = insert(node.left, data);
 		} else if (data > node.data) {
@@ -22,47 +25,49 @@ public class MyBST {
 		}
 		return node;
 	}
-	
+
 	public void inOrder(MyBSTNode root) {
 		if (root != null) {
 			inOrder(root.left);
-			System.out.print(root.data+" ");
+			System.out.print(root.data + " ");
 			inOrder(root.right);
 		}
 	}
-	
+
 	public void preOrder(MyBSTNode root) {
 		if (root != null) {
-			System.out.print(root.data+" ");
+			System.out.print(root.data + " ");
 			preOrder(root.left);
 			preOrder(root.right);
 		}
 	}
-	
+
 	public void postOrder(MyBSTNode root) {
 		if (root != null) {
 			postOrder(root.left);
 			postOrder(root.right);
-			System.out.print(root.data+" ");
+			System.out.print(root.data + " ");
 		}
 	}
-	
+
 	private void printInOrder() {
 		System.out.print("InOrder -> ");
 		inOrder(root);
 		System.out.println();
 	}
+
 	private void printPreOrder() {
 		System.out.print("PreOrder -> ");
 		preOrder(root);
 		System.out.println();
 	}
+
 	private void printPostOrder() {
 		System.out.print("PostOrder -> ");
 		postOrder(root);
 		System.out.println();
 	}
-	
+
 	private boolean search(MyBSTNode root, int data) {
 		if (root == null) {
 			return false;
@@ -75,7 +80,7 @@ public class MyBST {
 			return search(root.right, data);
 		}
 	}
-	
+
 	private boolean search(int data) {
 		return search(root, data);
 	}
@@ -88,7 +93,7 @@ public class MyBST {
 		}
 		return getLeastFromRightSubTree(node.left);
 	}
-	
+
 	private MyBSTNode deleteFoundObject(MyBSTNode node) {
 		if (node.left == null && node.right == null)
 			return null;
@@ -108,7 +113,7 @@ public class MyBST {
 			return node;
 		}
 	}
-	
+
 	private MyBSTNode deleteElement(MyBSTNode root, int data) {
 		if (root.data == data) {
 			return deleteFoundObject(root);
@@ -119,16 +124,59 @@ public class MyBST {
 		}
 		return root;
 	}
-	
+
 	private void delete(int data) {
 		if (search(data)) {
 			deleteElement(root, data);
-			System.out.println("Deleted -> "+ data);
+			System.out.println("Deleted -> " + data);
 		} else {
-			System.out.println("Element "+ data +" not found to delete");
+			System.out.println("Element " + data + " not found to delete");
 		}
 	}
+
+	private int getLeastNodeFromRightSubTree(MyBSTNode node) {
+		while (node != null && node.left != null) {
+			node = node.left;
+		}
+		return node.data;
+	}
+
+	private MyBSTNode deleteNode(MyBSTNode root, int data) {
+		if (data < root.data) {
+			root.left = deleteNode(root.left, data);
+		} else if (data > root.data) {
+			root.right = deleteNode(root.right, data);
+		} else {
+			if (root.left == null && root.right == null)
+				return null;
+			else if (root.left == null)
+				return root.right;
+			else if (root.right == null)
+				return root.left;
+			else {
+				int lessRightTreeData = getLeastNodeFromRightSubTree(root.right);
+				root.data = lessRightTreeData;
+				root.right = deleteNode(root.right, lessRightTreeData);
+			}
+		}
+		return root;
+	}
+
+	private void deleteNode(int data) {
+		root = deleteNode(root, data);
+	}
 	
+	private int getMinValue(MyBSTNode node) {
+		while (node != null && node.left != null) {
+			node = node.left;
+		}
+		return node.data;
+	}
+	
+	public int minValue() {
+		return getMinValue(root);
+	}
+
 	/*
 	 * 			   10
 	 * 			/      \
@@ -140,7 +188,7 @@ public class MyBST {
 	 */
 	public static void main(String[] args) {
 		MyBST bstObj = new MyBST();
-		//INSERT
+		// INSERT
 		bstObj.insert(10);
 		bstObj.insert(3);
 		bstObj.insert(18);
@@ -152,29 +200,35 @@ public class MyBST {
 		bstObj.insert(17);
 		bstObj.insert(19);
 		bstObj.insert(27);
-		
-		//TRAVERSE
+
+		// TRAVERSE
 		bstObj.printInOrder();
 		bstObj.printPreOrder();
 		bstObj.printPostOrder();
-		
-		//SEARCH
+
+		// SEARCH
 		System.out.println(bstObj.search(10) ? "found -> 10" : "Not found -> 10");
-		
-		//DELETE
-		bstObj.delete(10);
-		
-		//TRAVERSE
+
+		// DELETE
+//		bstObj.delete(10);
+
+		// DELETE BY OPTIMISTIC WAY
+		bstObj.deleteNode(10);
+
+		// TRAVERSE
 		bstObj.printInOrder();
+		
+		System.out.println("Minimum value : "+ bstObj.minValue());
 	}
 }
 
 class MyBSTNode {
 	MyBSTNode left, right;
 	int data;
+
 	public MyBSTNode(int data) {
 		this.data = data;
 		this.left = this.right = null;
 	}
-	
+
 }
