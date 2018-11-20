e1050972f350db794acb6b0a4a5b53a89b447671
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2018-11-21 00:50:19 +0530

    Update ConvertLevelOrderTraversalToBST.java

--- src/binarySearchTree/ConvertLevelOrderTraversalToBST.java
+++ src/binarySearchTree/ConvertLevelOrderTraversalToBST.java
@@ -7,18 +7,33 @@ public class ConvertLevelOrderTraversalToBST {
 	public static void main(String[] args) {
 		int[] arr = {7, 4, 12, 3, 6, 8, 1, 5, 10};
 		ConvertLevelOrderTraversalToBST obj = new ConvertLevelOrderTraversalToBST();
-		obj.constructBSTByLeelOrder(arr);
+		obj.doOperation(arr);
 	}
 	
+	private void doOperation(int[] arr) {
+		Queue<MyBSTNode> queueObj = new LinkedList<>();
+		for (int i = 0; i < arr.length; i++) {
+			queueObj.add(new MyBSTNode(i));
+		}
+		//constructBSTByLevelOrder(queueObj, 0, Integer.MAX_VALUE);
+	}
+
 //	     7        
 //	    / \       
 //	   4   12      
 //	  / \  /     
 //	 3   6 8    
 //	/   /   \
-//	1  5     10
+// 1   5     10
 
-	private MyBSTNode constructBSTByLeelOrder(int[] arr) {
+	/*private MyBSTNode constructBSTByLevelOrder(Queue<MyBSTNode> queueObj, int start, int end) {
+		MyBSTNode root = queueObj.poll();
+		if (roo) {
+			
+		}
+	}*/
+	
+	/*private MyBSTNode constructBSTByLeelOrder(int[] arr) {
 		Queue<MyBSTNode> queueObj = new LinkedList<>();
 		MyBSTNode root = new MyBSTNode(arr[0]);
 		queueObj.add(new MyBSTNode(-1));
@@ -28,5 +43,5 @@ public class ConvertLevelOrderTraversalToBST {
 			
 		}
 		return root;
-	}
+	}*/
 }
