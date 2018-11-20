19d57f788b17a4ba052e79018531e34a4cb7184c
Manohar Sambali <42957019+Manohar51@users.noreply.github.com>
2018-11-20 22:34:00 +0530

    Update ConvertLevelOrderTraversalToBST.java

--- src/binarySearchTree/ConvertLevelOrderTraversalToBST.java
+++ src/binarySearchTree/ConvertLevelOrderTraversalToBST.java
@@ -1,8 +1,32 @@
 package binarySearchTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ConvertLevelOrderTraversalToBST {
 	public static void main(String[] args) {
 		int[] arr = {7, 4, 12, 3, 6, 8, 1, 5, 10};
 		ConvertLevelOrderTraversalToBST obj = new ConvertLevelOrderTraversalToBST();
+		obj.constructBSTByLeelOrder(arr);
+	}
+	
+//	     7        
+//	    / \       
+//	   4   12      
+//	  / \  /     
+//	 3   6 8    
+//	/   /   \
+//	1  5     10
+
+	private MyBSTNode constructBSTByLeelOrder(int[] arr) {
+		Queue<MyBSTNode> queueObj = new LinkedList<>();
+		MyBSTNode root = new MyBSTNode(arr[0]);
+		queueObj.add(new MyBSTNode(-1));
+		queueObj.add(root);
+		for (int i = 1; i < arr.length; i++) {
+			queueObj.poll();
+			
+		}
+		return root;
 	}
 }
