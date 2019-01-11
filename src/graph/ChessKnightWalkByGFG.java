29806685db095287fd94b400ca9b94ca5b4e34e0
Manohar51 <manohar.glimpse@gmail.com>
2019-01-11 10:54:33 +0530

    commit in graph

--- src/graph/ChessKnightWalkByGFG.java
+++ src/graph/ChessKnightWalkByGFG.java
@@ -48,7 +48,7 @@ class Node {
 	}
 };
 
-public class ChessKnightWalk {
+public class ChessKnightWalkByGFG {
 	// Below arrays details all 8 possible movements
 	// for a knight
 	private static int row[] = { 2, 2, -2, -2, 1, 1, -1, -1 };
